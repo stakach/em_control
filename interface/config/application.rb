e904e8a7b8079fbe2e28b1721f6c8556cffc2f46
Stephen von Takach <steve@advancedcontrol.com.au>
2012-02-23 20:35:08 +1100

    bump asset version

--- interface/config/application.rb
+++ interface/config/application.rb
@@ -46,7 +46,7 @@ module Interface
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.5'
+    config.assets.version = '1.6'
   end
 end
 
