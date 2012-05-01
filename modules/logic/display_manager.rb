247b1fb72ea06ebad8525d4279ed1cd466125262
Stephen von Takach <steve@advancedcontrol.com.au>
2012-05-01 15:34:23 +1000

    display manager logic added for managing signage displays

---
+++ modules/logic/display_manager.rb
@@ -0,0 +1,70 @@
+#
+#
+# Manages displays in a digital signage network
+# => number == number of displays
+# => power_up and power_down are the cron job formatted times for start up and shutdown
+# => displayX == the name of the display at that load order
+#
+
+class DisplayManager < Control::Logic
+
+	def on_load
+		update_settings
+		schedule.every('1h') do
+			update_settings
+		end
+	end
+	
+	
+	def all_on
+		for i in 1..self[:number]
+			system["Display#{i}"].power(On)
+		end
+	end
+	
+	
+	def all_off
+		for i in 1..self[:number]
+			system["Display#{i}"].power(Off)
+		end
+	end
+	
+	
+	protected
+
+	def update_settings
+		self[:number] = setting(:number)
+		
+		for i in 1..self[:number]
+			self["display#{i}"] = setting("display")
+		end
+		
+		if @power_up_time.nil? || @power_up_time != setting(:power_up)
+			@power_up_time = setting(:power_up)
+			
+			@power_up.unschedule unless @power_up.nil?
+			
+			if @power_up_time != 'false'
+				@power_up = schedule.every(@power_up_time) do
+					logger.debug "-- Powering on the displays"
+					all_on
+				end
+			end
+		end
+		
+		if @power_down_time.nil? || @power_down_time != setting(:power_down)
+			@power_down_time = setting(:power_down)
+			
+			@power_down.unschedule unless @power_down.nil?
+			
+			if @power_down_time != 'false'
+				@power_down = schedule.every(@power_down_time) do
+					logger.debug "-- Powering off the displays"
+					all_off
+				end
+			end
+		end
+	end
+
+end
+
